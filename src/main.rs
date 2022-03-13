8ac344ea2be7b2fd789df89aa1f270b2ff4c725a
Maxime Bouillot <maxime.bouillot@withings.com>
2022-03-13 20:02:50 +0100

    Added proper support for big-endian.

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,11 @@ use unicode_converter::utf_32::Utf32;
 
 fn main() {
     let arg = Args::parse();
-    let decoded_message: Utf32 = match try_to_read_with_encoding(&arg.input_file, &arg.decoding_input, false) {
+    let input_file = match &arg.input_file as &str {
+        "-" => "/dev/stdin",
+        x => x,
+    };
+    let decoded_message: Utf32 = match try_to_read_with_encoding(input_file, &arg.decoding_input) {
         Some(x) => match x {
             Ok(y) => match y {
                 Ok(z) => z,
@@ -29,7 +33,7 @@ fn main() {
             std::process::exit(1);
         },
     };
-    let encoded_stream = match try_to_encode_data(&decoded_message, &arg.encoding_output, false) {
+    let encoded_stream = match try_to_encode_data(&decoded_message, &arg.encoding_output) {
         Some(x) => x,
         None => {
             eprintln!("Error, unknown output encoding.");
@@ -49,11 +53,11 @@ fn main() {
 /// returns it converted to UTF-32. The results are encapsulated the same was
 /// as the from_file function but in an option block where Node is returned if
 /// the type is not known
-fn try_to_read_with_encoding(filename: &str, encoding: &str, big_engian: bool)  -> Option<Result<Result<Utf32, UnicodeEncodingError>, std::io::Error>> {
+fn try_to_read_with_encoding(filename: &str, encoding: &str)  -> Option<Result<Result<Utf32, UnicodeEncodingError>, std::io::Error>> {
     macro_rules! ttrwe_case {
-        ($type: ty) => {
+        ($type: ty, $big_engian: expr) => {
             {
-                let encoded = <$type>::from_file(filename, big_engian);
+                let encoded = <$type>::from_file(filename, $big_engian);
                 match encoded {
                     Ok(x) => match x {
                         Ok(y) => Some(Ok(Ok(y.to_utf_32()))),
@@ -65,9 +69,11 @@ fn try_to_read_with_encoding(filename: &str, encoding: &str, big_engian: bool)  
         }
     }
     match encoding {
-        "UTF-8" => ttrwe_case!(Utf8),
-        "UTF-16" => ttrwe_case!(Utf16),
-        "UTF-32" => ttrwe_case!(Utf32),
+        "UTF-8" => ttrwe_case!(Utf8, false),
+        "UTF-16" => ttrwe_case!(Utf16, false),
+        "UTF-32" => ttrwe_case!(Utf32, false),
+        "UTF-16_be" => ttrwe_case!(Utf16, true),
+        "UTF-32_be" => ttrwe_case!(Utf32, true),
         _ => None,
     }
 }
@@ -75,37 +81,47 @@ fn try_to_read_with_encoding(filename: &str, encoding: &str, big_engian: bool)  
 /// Try to convert an UTF-32 in the encoding given as an argument string.
 /// If the encoding is not valid, None is returned. Then, the data is converted
 /// to a string of bytes.
-fn try_to_encode_data(utf32: &Utf32, encoding: &str, big_engian: bool) -> Option<Vec<u8>> {
+fn try_to_encode_data(utf32: &Utf32, encoding: &str) -> Option<Vec<u8>> {
     macro_rules! tted_case {
-        ($type: ty) => {
-            Some(<$type>::from_utf_32(utf32).to_bytes(big_engian))
+        ($type: ty, $big_engian: expr) => {
+            Some(<$type>::from_utf_32(utf32).to_bytes($big_engian))
         }
     }
     match encoding {
-        "UTF-8" => tted_case!(Utf8),
-        "UTF-16" => tted_case!(Utf16),
-        "UTF-32" => tted_case!(Utf32),
+        "UTF-8" => tted_case!(Utf8, false),
+        "UTF-16" => tted_case!(Utf16, false),
+        "UTF-32" => tted_case!(Utf32, false),
+        "UTF-16_be" => tted_case!(Utf16, true),
+        "UTF-32_be" => tted_case!(Utf32, true),
         _ => None,
     }
 }
 
-/// A tool to convert Unicode encodings
+/// A tool to convert Unicode text files between multiple Unicode encodings.
+/// The available encodings are:
+/// * UTF-8
+/// * UTF-16
+/// * UTF-32
+///
+/// By default, the data is assumed to be little-endian, but for encodings with
+/// multi-byte words such as UTF-16 or UTF-32, you can add the `_be` suffix to
+/// indicate that you want to work with big-endian data.
 #[derive(Parser, Debug)]
 #[clap(about, long_about = None)]
 struct Args {
-    /// Input file used as input. You can use `-` if you mean `/dev/stdin`.
+    /// Input file used as input. You can use `-` if you mean `/dev/stdin`
     #[clap(short, long)]
     input_file: String,
 
-    /// Input file encoding. Could be `UTF-8`, `UTF-16` or `UTF-32`.
+    /// Input file encoding
     #[clap(short, long)]
     decoding_input: String,
 
-    /// Output file, default to `/dev/stdout`.
+    /// Output file
     #[clap(short, long, default_value = "/dev/stdout")]
     output_file: String,
 
-    /// Input file encoding. Could be `UTF-8`, `UTF-16` or `UTF-32`.
+    /// Output file encoding
     #[clap(short, long)]
     encoding_output: String,
 }
